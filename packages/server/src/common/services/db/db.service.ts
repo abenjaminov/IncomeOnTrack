b59232c65167c4d923f357d6268c2fba5a5ba3f0
Asaf Benjaminov <abenjaminov@gmail.com>
2023-07-26 23:37:26 +0300

    dialect

--- packages/server/src/common/services/db/db.service.ts
+++ packages/server/src/common/services/db/db.service.ts
@@ -11,6 +11,11 @@ export class DBService implements IDBService {
             host: process.env.DB_HOST,
             database: process.env.DB_NAME,
             dialect: 'postgres',
+            dialectOptions: {
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            },
             username: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             port: Number(process.env.DB_PORT),
