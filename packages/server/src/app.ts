78fd10baefe16a1d7fdda2fbebb4ba04eb102480
Asaf Benjaminov <abenjaminov@gmail.com>
2023-07-26 22:16:26 +0300

    build and start

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -5,7 +5,7 @@ import { createServer } from './config/create-server';
 const app = createServer();
 
 const { PORT } = process.env;
-
+console.log(process.env)
 app.listen(process.env.PORT || 3000, () => {
     console .info(`Server listening on ${PORT}`);
 });
