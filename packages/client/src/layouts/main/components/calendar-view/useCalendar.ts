4878e7f39b263877f31e643f80707baab854864c
Asaf Benjaminov <abenjaminov@gmail.com>
2023-07-29 02:25:28 +0300

    or number

--- packages/client/src/layouts/main/components/calendar-view/useCalendar.ts
+++ packages/client/src/layouts/main/components/calendar-view/useCalendar.ts
@@ -1,6 +1,7 @@
 import {useApiQuery} from "@shared/hooks/useApi";
 import {ICalendarMonthView, SessionState} from "@income-on-track/shared";
 import {CacheKeys} from "@shared/types";
+import {getMonth} from "date-fns";
 
 export const useCalendar = (year: number, monthIndex: number, clientId?: string) => {
   const { data } = useApiQuery<unknown, ICalendarMonthView>(`calendar/${year}/${monthIndex}${clientId ? `/${clientId}` : ''}`, {
@@ -22,7 +23,7 @@ export const useCalendar = (year: number, monthIndex: number, clientId?: string)
   }
 
   const thisMonthDebt = data?.weeks.reduce((acc, week) => {
-    return acc + week.days.reduce((acc, day) => {
+    return acc + week.days.filter(x => x.month === monthIndex).reduce((acc, day) => {
       return acc + day.sessions.filter(x => x.sessionState === SessionState.debt).reduce((acc, session) => {
         return acc + session.payment
       }, 0)
@@ -30,7 +31,7 @@ export const useCalendar = (year: number, monthIndex: number, clientId?: string)
   }, 0)
 
   const thisMonthForecast = data?.weeks.reduce((acc, week) => {
-    return acc + week.days.reduce((acc, day) => {
+    return acc + week.days.filter(x => x.month === monthIndex).reduce((acc, day) => {
       return acc + day.sessions.reduce((acc, session) => {
         return acc + session.payment
       }, 0)
@@ -38,7 +39,7 @@ export const useCalendar = (year: number, monthIndex: number, clientId?: string)
   }, 0)
 
   const thisMonthIncome = data?.weeks.reduce((acc, week) => {
-    return acc + week.days.reduce((acc, day) => {
+    return acc + week.days.filter(x => x.month === monthIndex).reduce((acc, day) => {
       return acc + day.sessions.filter(x => x.sessionState === SessionState.payed).reduce((acc, session) => {
         return acc + session.payment
       }, 0)
